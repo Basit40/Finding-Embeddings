6683ea86ab1c44810b61a123cb1412ff75435701
Basit <Belhangari@gmail.com>
2025-12-19 10:06:23 -0500

    Hello from lenovo

--- homework.py
+++ homework.py
@@ -1,10 +1,12 @@
 import time
 print("Start", flush=True)
+t=[]
 for i in range(5):
     print(f"Line {i}", flush=True)
-    time.sleep(0.5)
+    time.sleep(1)
+    t.append( i)
 print("End", flush=False)
-
+print ("time_sleep=",t)
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -15,4 +17,6 @@ print(np.array([1,2,3]))
 x=np.random.rand(5)
 
 plt.stem(x)
+
+plt.title("A simple plot")
 plt.show()
